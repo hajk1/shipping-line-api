877458fd76e408430fbf5c67b8eff1305ad819f8
ArturSlimac <artur.nenakhov@student.hogent.be>
2026-02-14 16:51:45 +0100

    Bug fixing with creating and deleting freight orders in tests

--- src/test/java/com/shipping/freightops/controller/FreightOrderControllerTest.java
+++ src/test/java/com/shipping/freightops/controller/FreightOrderControllerTest.java
@@ -25,6 +25,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Integration test for {@link FreightOrderController}.
@@ -34,6 +35,7 @@ import org.springframework.test.web.servlet.MockMvc;
  */
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 class FreightOrderControllerTest {
 
   @Autowired private MockMvc mockMvc;
